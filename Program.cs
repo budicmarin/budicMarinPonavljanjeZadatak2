807ec698bad49814605539857e8f7d85b45fa643
budicmarin <55134480+budicmarin@users.noreply.github.com>
2020-11-12 10:18:47 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace budicMarinPonavljanjeZadatak2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Upiši niz znakova");
+            string nizZnakova=Console.ReadLine();
+            nizZnakova=Regex.Replace(nizZnakova," ","_");
+            Console.WriteLine(nizZnakova);
+            Console.ReadKey();
+        }
+    }
+}
